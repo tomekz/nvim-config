e979704f6fbb6f5617b5a09a9728efa8a43ae0c7
Tomasz Zadrożny <zadrozny.tomasz@gmail.com>
2023-11-08 11:40:21 +0100

    feat: customize ollama plugin params

--- lua/user/ollama.lua
+++ lua/user/ollama.lua
@@ -3,10 +3,20 @@ if not status_ok then
   return
 end
 
-gen.model = "llama2" -- default 'mistral:instruct'
+gen.model = "codellama" -- default 'mistral:instruct'
 
 gen.prompts["Fix_Code"] = {
   prompt = "Fix the following code. Only ouput the result in format ```$filetype\n...\n```:\n```$filetype\n$text\n```",
-  replace = true,
+  -- replace = true,
   extract = "```$filetype\n(.-)```",
+}
+
+gen.prompts["Change_Code"] = {
+  prompt = "Regarding the following code, $input, only ouput the result in format ```$filetype\n...\n```:\n```$filetype\n$text\n```",
+  -- replace = true,
+  extract = "```$filetype\n(.-)```",
+}
+
+gen.prompts["Review_Code"] = {
+  prompt = "Review the following code and make concise suggestions:\n```$filetype\n$text\n```",
 }
